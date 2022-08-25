8395efed93aa3521f510b484b96c2b776c56be7b
RaphaelBento <rafhaesilvalbento@gmail.com>
2022-08-25 17:25:39 -0300

    Api funcionando falta implementação com entity framework

---
+++ ApiDotNetFrameWork/Controllers/ClientesController.cs
@@ -0,0 +1,36 @@
+﻿using ApiDotNetFrameWork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiDotNetFrameWork.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+        
+
+        public List<Cliente> Get()
+        {
+            return clientes;
+        }
+
+        public void Post(int id,string nome ,string sobreNome)
+        {
+            if (!string.IsNullOrWhiteSpace(nome))
+            {
+                clientes.Add(new Cliente(id,nome,sobreNome));
+            }
+            
+        }
+
+        public void Delete(string nome)
+        {
+            clientes.RemoveAt(clientes.IndexOf(clientes.First(x => x.Nome.Equals(nome))));
+        }
+
+    }
+}
---
+++ ApiDotNetFrameWork/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiDotNetFrameWork.Models
+{
+    public class Cliente
+    {
+       
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+
+        public Cliente(int id,string nome, string sobreNome)
+        {
+            Id = id;
+            Nome = nome;
+            SobreNome = sobreNome;
+        }
+      
+    }
+}
